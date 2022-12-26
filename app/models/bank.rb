2ea72eba45888a8a4449d3d532da85725c9dbd90
royne <royne.chaves@gmail.com>
2022-12-26 21:07:17 +0000

    add bank model

---
+++ app/models/bank.rb
@@ -0,0 +1,3 @@
+class Bank < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'banks#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :banks
+  devise_for :users
 end
