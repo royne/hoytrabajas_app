1765bb9a1b6848d17b48d3023380d10f623046ea
royne <royne.chaves@gmail.com>
2022-12-28 01:49:54 +0000

    add validation nit

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -2,10 +2,13 @@ class Provider < ApplicationRecord
   has_one :account
   has_one :bank, through: :account
 
+  VALID_NIT_REGEX = { reg1: /^([0-9]{9}-[0-9]{1})?$/, reg2: /^([0-9]{9})?$/ }
+
   validates :name, presence: true
   validates :contact_name, presence: true, length: { maximum: 10 }
-
-  accepts_nested_attributes_for :account
+  validate :validate_nit
+  
+  accepts_nested_attributes_for :account, update_only: true
 
   self.per_page = 10
 
@@ -17,4 +20,12 @@ class Provider < ApplicationRecord
     account&.number
   end
 
+  private
+
+    def validate_nit
+      unless VALID_NIT_REGEX[:reg1].match(self.nit) || VALID_NIT_REGEX[:reg2].match(self.nit)
+        errors.add(:nit, :invalid)
+      end
+    end
+    
 end
